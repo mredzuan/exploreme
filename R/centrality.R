532f89e5e85cc2837d7193eb24014ba36f1ddcdb
redz <redzu.m@gmail.com>
2023-11-02 11:20:57 +0800

    Update pkg function and metadata

--- R/centrality.R
+++ R/centrality.R
@@ -1,12 +1,31 @@
-#Generic function for centerality
+#Generic function for centrality
 
+#' S3 Generic function for statistic centrality
+#'
+#' @param x Support data.frame and vector object class
+#'
+#
 centrality <- function(x, ...){
   UseMethod("centrality")
 }
 
 
-#S3 method for data.frame
-centrality.data.frame <- function(x, remove_na = TRUE, ...) {
+
+#' Function to generate statistical centrality metrics for data.frame class object
+#'
+#' @param x Data Frame.
+#' @param remove_na a logical evaluating to TRUE or FALSE indicating whether NA values should be stripped before the computation proceeds.
+#'
+#' @return List object that summarize all numeric columns in data.frame object into mean, median and mode
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' centrality_iris <- centrality(iris)
+#'
+#' }
+centrality.data.frame <- function(x, remove_na = TRUE) {
 
   numeric_columns <- sapply(x, is.numeric)
   numeric_df <- x[, numeric_columns]
@@ -15,7 +34,7 @@ centrality.data.frame <- function(x, remove_na = TRUE, ...) {
     mean_val <- mean(col, na.rm = remove_na)
     median_val <- median(col, na.rm = remove_na)
 
-    mode_val <- DescTools::Mode(col, na.rm = remove_na)
+    mode_val <- Mode(col, na.rm = remove_na)
     list(mean = mean_val, median = median_val, mode = mode_val)
   })
 }
@@ -25,9 +44,9 @@ centrality.data.frame <- function(x, remove_na = TRUE, ...) {
 
 #Test---------
 
-df_test <- data.frame(
-  Name = c("Alice", "Bob", "Charlie", "David", "Eve", "Redz"),
-  Age = c(25, NA, 30, 28, 22, 22),
-  Height = c(160, 175, 180, NA, 165, 165),
-  Weight = c(55, 75, NA, 68, 60, 60)
-)
+# df_test <- data.frame(
+#   Name = c("Alice", "Bob", "Charlie", "David", "Eve", "Redz"),
+#   Age = c(25, NA, 30, 28, 22, 22),
+#   Height = c(160, 175, 180, NA, 165, 165),
+#   Weight = c(55, 75, NA, 68, 60, 60)
+# )
---
+++ R/exploreme-package.R
@@ -0,0 +1,8 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom DescTools Mode
+#' @importFrom stats median
+## usethis namespace: end
+NULL
