f1909bbc0ad1c1d7caa4c634a2bccdd730c741cc
redz <redzu.m@gmail.com>
2023-11-02 15:45:47 +0800

    Update centrality generic and method functions

--- R/centrality.R
+++ R/centrality.R
@@ -1,8 +1,22 @@
-#Generic function for centrality
-
 #' S3 Generic function for statistic centrality
 #'
-#' @param x Support data.frame and vector object class
+#' @param x data.frame or vector type object class
+#' @param ... Additional arguments affecting the centrality metrics produced.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' #Centrality for data.frame object
+#'
+#' centrality_iris <- centrality(iris)
+#'
+#'
+#' #Centrality for numeric object
+#'
+#' centrality_num <- centrality(c(1, 2, 3, 3, 4, 4, 4))
+#'
+#' }
 #'
 #
 centrality <- function(x, ...){
@@ -18,13 +32,7 @@ centrality <- function(x, ...){
 #'
 #' @return List object that summarize all numeric columns in data.frame object into mean, median and mode
 #' @export
-#'
-#' @examples
-#' \dontrun{
-#'
-#' centrality_iris <- centrality(iris)
-#'
-#' }
+
 centrality.data.frame <- function(x, remove_na = TRUE) {
 
   numeric_columns <- sapply(x, is.numeric)
@@ -34,11 +42,45 @@ centrality.data.frame <- function(x, remove_na = TRUE) {
     mean_val <- mean(col, na.rm = remove_na)
     median_val <- median(col, na.rm = remove_na)
 
-    mode_val <- Mode(col, na.rm = remove_na)
-    list(mean = mean_val, median = median_val, mode = mode_val)
+    mode_val <- DescTools::Mode(col, na.rm = remove_na)
+    res <- list(mean = mean_val, median = median_val, mode = mode_val)
+    return(res)
   })
 }
 
+
+
+
+
+
+#' Function to generate statistical centrality metrics for numeric class object
+#'
+#' @param x Numeric vector
+#' @param remove_na a logical evaluating to TRUE or FALSE indicating whether NA values should be stripped before the computation proceeds.
+#'
+#' @return List object that summarize numeric vector into mean, median and mode
+#' @export
+centrality.numeric <- function(x, remove_na = TRUE) {
+
+  mean_val <- mean(x, na.rm = remove_na)
+  median_val <- median(x, na.rm = remove_na)
+  mode_val <- DescTools::Mode(x, na.rm = remove_na)
+
+  res <- list(mean = mean_val, median = median_val, mode = mode_val)
+  return(res)
+
+}
+
+
+
+
+#' Default method for centrality generic function
+#'
+#' @param x Object
+#' @export
+centrality.default <- function(x){
+  stop("Please input object with data.frame or numeric class!")
+}
 
 
 
@@ -50,3 +92,7 @@ centrality.data.frame <- function(x, remove_na = TRUE) {
 #   Height = c(160, 175, 180, NA, 165, 165),
 #   Weight = c(55, 75, NA, 68, 60, 60)
 # )
+
+
+
+
