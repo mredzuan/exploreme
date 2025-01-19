1c7cc14a97894f224f58a7421daf691e6adebcbb
Redz <redzu.m@gmail.com>
2025-01-20 06:50:28 +0800

    Update file meta data function

--- function/file_metadata_fun.R
+++ function/file_metadata_fun.R
@@ -7,16 +7,14 @@ file_meta_data <- function(file_path){
     file_size <- file.info(file_path)$size
     file_creation_time <- file.info(file_path)$ctime
     file_modification_time <- file.info(file_path)$mtime
-    file_access_time <- file.info(file_path)$atime
-    file_creator <- file.info(file_path)$uname
+    file_last_access_time <- file.info(file_path)$atime
 
     file_meta_data <- list(file_name = file_name,
                            file_extension = file_extension,
                            file_size = file_size,
                            file_creation_time = file_creation_time,
                            file_modification_time = file_modification_time,
-                           file_access_time = file_access_time,
-                           file_creator = file_creator)
+                           file_last_access_time = file_last_access_time)
 
     return(file_meta_data)
   }, error = function(e){
