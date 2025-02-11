215b97b2120c6fb4552f02f65280a1b444e660d1
Redz <redzu.m@gmail.com>
2025-02-12 07:17:33 +0800

    Update code to validate data import - not working

--- module/imported_data_obj_view_module.R
+++ module/imported_data_obj_view_module.R
@@ -1,3 +1,5 @@
+# TO DO
+# To refactor the code due to display message when no data is imported
 
 mod_import_data_obj_view_ui <- function(id) {
 
@@ -7,7 +9,7 @@ mod_import_data_obj_view_ui <- function(id) {
 
     h2("Imported File Information"),
     br(),
-    dataTableOutput(ns("object_info_table"))
+    uiOutput(ns("file_info_ui"))
 
   )
 }
@@ -18,7 +20,7 @@ mod_import_data_obj_view_server <- function(id, input_dataset){
   moduleServer(id, function(input, output, session) {
 
     object_info <- reactive({
-      req(input_dataset())
+      #req(input_dataset())
 
       df <- input_dataset()
 
@@ -29,6 +31,19 @@ mod_import_data_obj_view_server <- function(id, input_dataset){
         stringsAsFactors = FALSE
       )
     })
+
+
+
+    output$file_info_ui <- renderUI({
+
+      if (is.null(object_info())) {
+        tags$p("Nothing to display, please import a dataset!",
+               style = "color: red; font-weight: bold;")
+      } else {
+        DT::dataTableOutput(session$ns("object_info_table"))
+      }
+    })
+
 
 
     output$object_info_table <- renderDataTable({
